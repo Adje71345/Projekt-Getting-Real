cde66ce4c4d89b7f71553baf2a1f46d346e979d1
Annette Jensen <annetteduedal@gmail.com>
2025-05-05 13:01:10 +0200

    Ny WPF

--- GettingRealWPF/MainWindow.xaml.cs
+++ GettingRealWPF/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GettingReal
+namespace GettingRealWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
