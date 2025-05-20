d2e1faacb13a60759bf12071abbe96f0549d4e2b
Lui <ddng72488@edu.ucl.dk>
2025-05-20 14:33:35 +0200

    Lui12

---
+++ GettingRealWPF/View/AddMaterialView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using GettingRealWPF.ViewModel;
+using GettingRealWPF.Model;
+
+namespace GettingRealWPF.View
+{
+    /// <summary>
+    /// Interaction logic for AddMaterialView.xaml
+    /// </summary>
+    public partial class AddMaterialView : UserControl
+    {
+        public AddMaterialView()
+        {
+            InitializeComponent();
+
+            // Sæt DataContext til ViewModel med korrekte repositories
+            DataContext = new AddMaterialViewModel(
+                new FileMaterialRepository("materials.txt"),
+                new FileInventoryItemRepository("inventoryitems.txt"),
+                new FileStorageRepository("storages.txt"));
+        }
+    }
+}
